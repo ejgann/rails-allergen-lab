835d873c52f4aac27ab54cd69eb7c7a69c7be04d
ejgann <aspentree79@gmail.com>
2019-11-26 16:49:05 -0500

    user params fun

--- allergen_lab/app/controllers/users_controller.rb
+++ allergen_lab/app/controllers/users_controller.rb
@@ -1,23 +1,44 @@
 class UsersController < ApplicationController
 
-   def create
-    @user = User.create(params[:id])
-    @user.save
-    redirect_to users_path
+    def index
+    @user = User.all 
+    end 
+
+    def create
+    user = User.create(user_strong_params)
+    redirect_to user_path(user)
     end
-    
+
+    def edit 
+        define_user
+    end
+
+    def update
+        @user.update(user_strong_params)
+        redirect_to (user_path(@user))
+    end 
+
     def new
         @user = User.new
     end 
 
     def show
-        @user = User.find(params[:id])
+        define_user
     end
+
+    def destroy
+     define_user.destroy
+     redirect_to users_path
+    end 
 
     private
 
     def define_user
- 
+        @user = User.find(params[:id])
     end
+
+    def user_strong_params 
+        params.require(:user).permit(:name)
+    end 
 
 end
